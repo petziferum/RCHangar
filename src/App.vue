4e010d0f22e04a337659c83486c8bab212953dc0
petziferum <daboarderpjb@gmail.com>
2023-12-12 19:06:37 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="appMain">
+  <RouterView />
+  </main>
+</template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+<style scoped>
+.appMain {
+  padding-top: 64px;
+  background-color: #5a664b;
+}
+</style>
