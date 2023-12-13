f0b3348986a9079c74b9af12ccae2c441f2cb816
petziferum <daboarderpjb@gmail.com>
2023-12-13 18:52:32 +0100

    add firebase login

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,58 @@
   <v-main class="appMain">
   <RouterView />
   </v-main>
+    <v-footer>
+      <div class="footer">
+    <div v-if="u" style="height: 200px">
+      fireUser: {{ u.email }}
+      <div>
+        <v-btn @click="logOut">logout</v-btn>
+      </div>
+    </div>
+      <div v-else>
+        <v-btn @click="registerWithGoogle">Login mit Google</v-btn>
+      </div>
+      </div>
+    </v-footer>
   </v-app>
 </template>
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import AppBar from '../src/components/common/AppBar.vue'
+import { onBeforeMount, ref } from 'vue'
+import { fireAuth, fireUser, registerWithGoogle, logOut as outlog } from '@/plugins/firesbaseConfig'
+import { onAuthStateChanged } from 'firebase/auth'
+import firebase from 'firebase/compat'
 
+const u = ref(fireUser)
+
+onBeforeMount(() => {
+  onAuthStateChanged(fireAuth, (user) => {
+    console.log("Authstate changed", user);
+if (user as firebase.User) {
+      u.value = user;
+    } else {
+      u.value= null;
+    }
+  });
+});
+function logOut() {
+  outlog();
+}
 </script>
 <style scoped>
+.footer {
+  height: 300px;
+  align-content: center;
+  margin-top: 5px;
+}
 .appMain {
+  z-index: 5;
   padding-top: 64px;
   background-color: #5a664b;
   border: 0px solid #00664b;
-  border-radius: 10px;
+  box-shadow: 0px 5px 8px -1px rgba(0,0,0,0.7);
+  border-radius: 20px;
   height: 100%;
   width: 100%;
 }
--- src/plugins/firesbaseConfig.ts
+++ src/plugins/firesbaseConfig.ts
@@ -1,7 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore/lite'
+import { getAuth, signOut, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGwO2YDTyZFJsiZlWgfgaqkXYUqCgHsHc",
@@ -17,6 +17,24 @@ const firebaseApp = initializeApp(firebaseConfig);
 const fireStore = getFirestore(firebaseApp);
 const fireBucket = getStorage(firebaseApp);
 const fireAuth = getAuth(firebaseApp);
+let fireUser = fireAuth.currentUser;
+export const registerWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      fireUser = result.user;
+    })
+    .catch((error) => {
+      console.error("Fehler beim login:", error.message);
+    });
+};
+export const logOut = () => {
+  signOut(fireAuth).then(() => {
+    fireUser = null;
+  });
+}
 
-export { fireStore, fireBucket, fireAuth };
+
+export { fireStore, fireBucket, fireAuth, fireUser };
 export default firebaseApp;
