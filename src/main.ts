53dfbeb6016310097ea3be0a2bed199065618f05
petziferum <daboarderpjb@gmail.com>
2023-12-13 19:50:25 +0100

    vuetify auslagern

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import vuetify from './plugins/vuetify';
+
 
 import App from './App.vue'
 import router from './router'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi,
-    },
-  },
-})
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
