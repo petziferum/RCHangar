eac105b7f3d74013b8f88e43974d1185a6bf5414
petziferum <daboarderpjb@gmail.com>
2023-12-15 21:00:03 +0100

    dialog

--- src/components/BaseDialog.vue
+++ src/components/BaseDialog.vue
@@ -3,13 +3,15 @@
   <template v-slot:activator="{ props }">
     <v-btn variant="outlined" prepend-icon="mdi-airplane" v-bind="props" @click="emitEvent">{{activatorText}}</v-btn>
   </template>
-  <v-card title="Dialog" text="Inhalt"></v-card>
+  <v-card :title="title">
+    <slot></slot>
+  </v-card>
 </v-dialog>
 </template>
 <script setup lang=ts>
 import { ref } from 'vue'
 
-const props = defineProps(["activatorText"]);
+const props = defineProps(["activatorText","title"]);
 const isOpen = ref(false);
 function emitEvent() {
   console.log("emitEvent");
--- src/views/WorkbenchView.vue
+++ src/views/WorkbenchView.vue
@@ -2,7 +2,22 @@
   <v-container fluid>
     <v-toolbar title="Werkbank">
       <v-spacer />
-      <BaseDialog activator-text="Flugzeug erstellen" />
+      <BaseDialog activator-text="Flugzeug erstellen" title="Name des Flugzeugs" ref="baseDialog">
+        <v-form ref="createPlaneForm" @submit.prevent="createPlane">
+          <v-card-text>
+            <v-text-field
+              label="Name des Flugzeugs"
+              :rules="filledRule"
+              v-model="newPlane.name"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn variant="text" color="red" @click="closeDialog = false">abbrechen</v-btn>
+            <v-btn variant="text" color="green" type="submit">weiter</v-btn>
+          </v-card-actions>
+        </v-form>
+      </BaseDialog>
       <v-spacer></v-spacer>
       <v-btn variant="outlined" elevation="2" class="bg-blue-accent-1">
         <v-icon icon="mdi-information-outline" />
@@ -19,6 +34,22 @@
 
 <script setup lang="ts">
 import BaseDialog from '@/components/BaseDialog.vue'
+import { ref } from 'vue'
+import Plane from '@/types/Plane'
+import { useUserStore } from '@/stores/userStore'
+
+const userStore = useUserStore();
+const filledRule = ref([(v) => v != null || "Name muss ausgefüllt sein"]);
+const newPlane = ref(Plane.createEmptyPlane());
+const baseDialog = ref(null);
+
+function createPlane() {
+
+}
+
+function closeDialog() {
+  baseDialog.value.isOpen = false;
+}
 </script>
 <style scoped>
 
