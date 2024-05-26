c9d9f7b0c688148814489abdc77b525fa72323ab
petziferum <daboarderpjb@gmail.com>
2024-05-26 22:20:59 +0200

    background

--- src/App.vue
+++ src/App.vue
@@ -59,11 +59,24 @@ function logOut() {
 .appMain {
   z-index: 5;
   padding: 101px;
-  background-color: #5a664b;
+  background: #5a664b url('https://firebasestorage.googleapis.com/v0/b/hangar-7334.appspot.com/o/images%2FHimmel.jpg?alt=media&token=c90e264f-699b-4add-b042-84d1b97c59f2') repeat-y center center;
+  background-size: 200%;
+  background-attachment: fixed;
   border: 0px solid #00664b;
   box-shadow: 0px 25px 20px -2px rgba(0,0,0,0.5);
   border-radius: 20px;
   height: 100%;
   width: 100%;
+}
+.bg {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url( 'https://cdn.pixabay.com/photo/2016/03/26/13/09/organic-1280537_1280.jpg') no-repeat center center;
+  background-size: cover;
+  background-color: red;
+  transform: scale(1.1);
 }
 </style>
